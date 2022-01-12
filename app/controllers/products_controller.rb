0f4cabbe73bbc01581a077afb72035201c1d8783
Corrie <corrieleech@gmail.com>
2022-01-12 11:55:52 -0600

    adds first and last product paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products
   end
+  def display_first
+    first = Product.first
+    render json: first
+  end
+  def display_last
+    last = Product.last
+    render json: last
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/store_front", controller: "products", action: "display_all"
+  get "/first_product", controller: "products", action: "display_first"
+  get "/newest_product", controller: "products", action: "display_last"
+  
+  
 end
